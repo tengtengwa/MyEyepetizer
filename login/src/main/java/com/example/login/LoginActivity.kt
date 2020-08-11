8bc4ad0d65ea25fefcb5efc9b980088c926067dd
tengtengwa <1511900943@qq.com>
2020-08-11 23:58:37 +0800

    LoginActivity的布局写得差不多了

--- login/src/main/java/com/example/login/LoginActivity.kt
+++ login/src/main/java/com/example/login/LoginActivity.kt
@@ -13,18 +13,16 @@ class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_login)
+        initViews()
+    }
 
-        val username = findViewById<EditText>(R.id.username)
-        val password = findViewById<EditText>(R.id.password)
-        val login = findViewById<Button>(R.id.login)
-        val loading = findViewById<ProgressBar>(R.id.loading)
+    private fun initViews() {
 
     }
 
-    companion object {
 
+    companion object {
         fun start() {
             ARouter.getInstance().build("/epetizer/loginActivity").navigation()
         }
