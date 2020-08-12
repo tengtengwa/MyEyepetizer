d3e8ce25396e7c9a5f5b2464e950b974ce589830
tengtengwa <1511900943@qq.com>
2020-08-12 17:55:20 +0800

    修复了在LoginActivity中获取不到标题栏引用的bug，是因为它被login布局所引用并且在其中也指定了id，删去一个即可

--- login/src/main/java/com/example/login/LoginActivity.kt
+++ login/src/main/java/com/example/login/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.example.login
 
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.example.base.BaseActivity
+import com.example.base.utils.logD
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.layout_title.*
 
 @Route(path = "/epetizer/loginActivity")
 class LoginActivity : BaseActivity() {
@@ -14,11 +17,16 @@ class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        initViews()
+
     }
 
-    private fun initViews() {
-
+    override fun setupViews() {
+        super.setupViews()
+        setStatusBarBackground(R.color.colorBlack)
+        layout_title.setBackgroundColor(ContextCompat.getColor(this, android.R.color.transparent))
+        iv_back.setImageResource(R.drawable.ic_close_white_24dp)
+        iv_share.visibility = View.INVISIBLE
+        tv_right_top.visibility = View.VISIBLE
     }
 
 
