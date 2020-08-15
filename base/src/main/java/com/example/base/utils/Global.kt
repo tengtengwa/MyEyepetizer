787e6372f1ee169e9aa01950133176e9d6b0644a
tengtengwa <1511900943@qq.com>
2020-08-15 18:31:48 +0800

    增加工具函数，修改登陆布局中控件的名称

--- base/src/main/java/com/example/base/utils/Global.kt
+++ base/src/main/java/com/example/base/utils/Global.kt
@@ -1,5 +1,28 @@
 package com.example.base.utils
 
 import android.util.Log
+import android.view.View
 
+/**
+ * 一些全局工具函数
+ */
+
+
+/**
+ * 懒人打印日志函数
+ */
 fun logD(TAG: String, message: String) = Log.d(TAG, message)
+
+
+/**
+ * 批量注册监听器的方法
+ *
+ * @param views 所有需要设置监听器的view对象
+ * @param block 每个view的onClick方法中要执行的代码段，应该由一个when语句根据不同的view执行不同的操作
+ */
+fun setOnClickListener(vararg views: View?, block: View.() -> Unit) {
+    //返回的是一个OnClickListener接口的实例，在onClick方法中调用block函数
+    val listener = View.OnClickListener { it.block() }
+    //遍历views，给它们设置监听器
+    views.forEach { it?.setOnClickListener(listener) }
+}
