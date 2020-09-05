83d3b9d8b590d5df993b1388a527f530b04f0067
tengtengwa <1511900943@qq.com>
2020-09-05 10:11:09 +0800

    部分Fragment持有一个MainViewModel对象用于接收刷新数据

--- main/src/main/java/com/example/main/notification/push/PushFragment.kt
+++ main/src/main/java/com/example/main/notification/push/PushFragment.kt
@@ -5,56 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.base.BaseFragment
+import com.example.main.MainViewModel
 import com.example.main.R
+import com.example.main.utils.EventObserver
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class PushFragment : BaseFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PushFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PushFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_fragment_push, container, false)
+        return onCreateView(inflater.inflate(R.layout.main_fragment_push, container, false))
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        observe()
+    }
+
+    private fun observe() {
+        mainViewModel.isRefreshNotificationPush.observe(viewLifecycleOwner, EventObserver {
+            TODO("在这里刷新数据")
+        })
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PushFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PushFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(param1: String, param2: String) = PushFragment()
     }
 }
